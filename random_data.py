5bfb4bd703a4b37c4af0077378ca22e327ecbc1a
Kunal <waghkunal1998@gmail.com>
2024-02-26 14:16:58 +0530

    new changes

--- random_data.py
+++ random_data.py
@@ -3,12 +3,18 @@ from datetime import date, datetime, timedelta
 
 
 def gen_date():
+    """
+    gen_date
+    """
     start_date = date(2000, 1, 1)
     end_date = date.today()
     random_date = start_date + timedelta(days=random.randint(0, (end_date - start_date).days))
     return random_date
 
 def create_assets(p):
+    """
+    create_assets
+    """
     li = []
     date_list = [ row["purchase_date"] for row in li ] if len(li) != 0 else []
     for i in range(p):
@@ -20,34 +26,56 @@ def create_assets(p):
 
 
 def final(p):
+    """
+    final
+    """
     result = []
     for i in range(1, p):
         assets = create_assets(i)
         result.extend(assets)
     return result
 
+def unique_id(data,assets,start_date,ids):
+    
+    
+    id = int(random.sample(ids, k=1)[0])
+    if len(data) == 0 :
+        return id
+    
+    for row in data :
+        if row["asset_id"] ==  id :
+            if row["start_date"] <=  start_date <= row["end_date"] :
+                return  unique_id(data,assets,start_date)
+            else : 
+                return id
+ 
 
 def rentals(p):
+    """
+    rentals
+    """
     rentals = []
     
     i =1
     while len(rentals) <= p :
         
-        dict = {}
+        data = {}
         assets = final(p)
         start = str(gen_date())
         end = str(gen_date())
-        ids = [row["id"] for row in assets]
-        date_list = [ row["start_date"] for row in rentals ] if len(rentals) != 0 else []      
+        
+        date_list = [ row["start_date"] for row in rentals ] if len(rentals) != 0 else [] 
+        ids = [row["id"] for row in assets]     
         now = datetime.now().strftime("%Y-%m-%d")
         if start < end and start < now  and start not in date_list :
-            dict = {
+             
+            data = {
                 "id": i,
-                "asset_id": int(random.sample(ids, k=1)[0]),
                 "start_date": start,
+                "asset_id": unique_id(data,assets,start,ids),
                 "end_date": end,
             }
-            rentals.append(dict)
+            rentals.append(data)
             i += 1      
     return rentals
 
@@ -55,12 +83,12 @@ def rentals(p):
 if __name__ == "__main__":
     print(rentals(100))
 
+
+
 # using Recursion
 
 # def gen_date():
-
-
-#         year = "2024"
+#         year = "2023"
 #         month_d = random.randint(1, 12)
 #         month = f'0{month_d}' if month_d <= 9 else str(month_d)
 #         day_d = random.randint(1, 28)
@@ -70,27 +98,64 @@ if __name__ == "__main__":
 
 
 # def create_assets(p,li):
-#     date_list =  [ row["purchase_date"] for row in li ]
-#     dict={}
+
+ 
+#     date_list =  [ row["purchase_date"] for row in li ] if len(li) != 0 else [] 
+#     record ={}
 #     now  =  datetime.now().strftime("%Y-%m-%d")
 #     date =  gen_date()
-#     if now > date  and not date_list in date :
-#             dict = { "id": p , "purchase_date" :date}
-#             li.append(dict)
+#     if now > date  and  date not in date_list :    
+#             record = { "id": p , "purchase_date" :date}
+#             li.append(record)
 #     else :
 #             create_assets(p,li)
 #     return li
 
-# def final():
+# def assets(p):
 #     li =[]
-#     for i in range(1,8):
-#        final=  create_assets(i,li)
-#        li.extend(final)
-
+#     for i in range(1,p):
+#       create_assets(i,li)    
 #     return li
 
+
+# def rentals(p):
+#     """
+#     rentals
+#     """
+#     rental = [] 
+    
+    
+
+
 # if __name__ == "__main__":
-#     print(final())
+#     print(assets())
+
+
+
+# from datetime import datetime
+
+# def create_assets(p, li):
+#     date_list = [row["purchase_date"] for row in li] if len(li) != 0 else []  
+#     record = {}
+#     now = datetime.now().strftime("%Y-%m-%d")
+#     date = gen_date()
+
+#     if now > date and date not in date_list:
+#         record = {"id": p, "purchase_date": date}
+#         li.append(record)
+#     else:
+#         create_assets(p, li)
+#     return li
+
+# def final():
+#     li = []
+#     for i in range(1, 8):
+#         create_assets(i, li)
+#     return li
+
+
+# print(final())
+
 
 
 # def result():
